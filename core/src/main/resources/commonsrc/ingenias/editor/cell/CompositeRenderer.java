2b75840437f7b3c6eab4d169cc946b8fceac53a0
escalope <jjgomez@fdi.ucm.es>
2015-05-28 08:00:09 +0200

    for rencording positions of entities positions

--- core/src/main/resources/commonsrc/ingenias/editor/cell/CompositeRenderer.java
+++ core/src/main/resources/commonsrc/ingenias/editor/cell/CompositeRenderer.java
@@ -1,14 +1,22 @@
 package ingenias.editor.cell;
 
+import java.awt.Component;
 import java.util.Map;
 
 import javax.swing.JComponent;
 
+import org.jgraph.JGraph;
+import org.jgraph.graph.CellView;
 import org.jgraph.graph.VertexRenderer;
 
 import ingenias.editor.entities.Entity;
+import ingenias.editor.entities.ViewPreferences.ViewType;
 
 public abstract class CompositeRenderer extends VertexRenderer{
 	
 	  public abstract JComponent getConcreteSubComponent(String fieldname, Entity ent,Map cellAttributes); 
+	  public abstract Component getRendererComponent(JGraph graph,
+				CellView view, boolean sel,
+				boolean focus, boolean preview, ViewType vt);
+	  
 }
